74d07233433bd69b6ed5ca9ed8f7b7dc8a69b791
Bart <barteqczech@gmail.com>
2023-08-15 18:42:02 +0200

    Signup works (no password encryption)

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,37 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const { body, validationResult } = require("express-validator");
 
 exports.users_read = asyncHandler(async (req, res) => {
   res.json({
     message: "users read",
   });
 });
+
+exports.users_create = [
+  body("email").escape(),
+  body("password").escape(),
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.json("There occurred errors while submitting the form.");
+    } else {
+      const userExists = await User.findOne({ email: req.body.email });
+      if (userExists) {
+        console.log(userExists);
+        res.json({
+          message: "user with this email exists",
+        });
+      } else {
+        const new_user = new User({
+          email: req.body.email,
+          password: req.body.password,
+        });
+        await new_user.save();
+        res.json({
+          message: "user successfully created",
+        });
+      }
+    }
+  }),
+];
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const userController = require('../controllers/userController');
 
 router.get('/', userController.users_read);
+router.post('/', userController.users_create);
 
 module.exports = router;
 
